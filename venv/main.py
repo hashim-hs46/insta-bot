51333c67bb58f12024fc2ad2e384fa4608bba5da
Hashim Shah <shah.hashim99@gmail.com>
2020-08-12 18:37:30 +0530

    first commit

---
+++ venv/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+class InstaBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://instagram.com")
+
+
+InstaBot()
